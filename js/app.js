32a7918275fd018940aeb4656c5f36b627975d04
kervines <kervinzero@gmail.com>
2024-03-26 22:54:47 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 import Countdown from './countdown.js';
 
 const contagem = document.querySelector('.contagem-regressiva');
-const contagemParaoDia = new Countdown('1 April 2024 23:59:59 GMT-0300');
+const contagemParaoDia = new Countdown('6 April 2024 23:59:59 GMT-0300');
 
 setInterval(() => {
   contagem.innerHTML = `
-  <div><span>${contagemParaoDia.total.days} </span><p>dias</p></div> 
-  <div><span>${contagemParaoDia.total.hours} </span><p>horas</p></div>
-  <div><span>${contagemParaoDia.total.minutes}</span><p>minutos</p></div>
-  <div><span>${contagemParaoDia.total.seconds}</span><p>segundos</p></div>`;
+  <div><span>${contagemParaoDia.total.days}</span><p>dias para o grande dia!</p></div> `;
 });
